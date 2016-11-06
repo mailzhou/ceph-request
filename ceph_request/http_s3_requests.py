dae4349c658399b07d49e3d6acf339f0b1f47766
yuliyang <wzyuliyang911@gmail.com>
2016-11-06 14:04:40 +0800

    增加s3 post接口
    Signed-off-by: yuliyang <wzyuliyang911@gmail.com>

--- ceph_request/http_s3_requests.py
+++ ceph_request/http_s3_requests.py
@@ -30,8 +30,34 @@ def s3_get(host='127.0.0.1', port='7480', cmd='/', access_key='', secret_key='',
 
 # TODO ADD POST METHOD
 # multi-part upload use it ,we will add it later
-def s3_post(host='127.0.0.1', port='7480', cmd='/', access_key='', secret_key='',show_dump = False):
-    pass
+def s3_post(host='127.0.0.1', port='7480', cmd='/', access_key='', secret_key='',headers=None,file=None,content=None,show_dump = False):
+    '''
+    post request use aws2
+    '''
+    if headers:
+        headers = json.loads(headers)
+    url = 'http://%s:%s%s' % (host, port, cmd)
+    response = None
+    if file:
+        with open(file, 'rb') as fin:
+            file_content = fin.read()
+        # upload object from file
+        response = requests.post(url, auth=S3Auth(access_key, secret_key, service_url=host + ":" + port),
+                                headers=headers, data=file_content)
+
+    elif content:
+        # upload object from content
+        response = requests.post(url, auth=S3Auth(access_key, secret_key, service_url=host + ":" + port),
+                                headers=headers, data=content)
+    else:
+        # create bucket
+        response = requests.post(url, auth=S3Auth(access_key, secret_key, service_url=host + ":" + port),
+                                headers=headers)
+    if show_dump:
+        data = dump.dump_all(response)
+        print(data.decode('utf-8'))
+    else:
+        print response.content
 
 # TODO ADD HEADER AND DATA SUPPORDED
 def s3_put(host='127.0.0.1', port='7480', cmd='/', access_key='', secret_key='',headers=None,file=None,content=None,show_dump = False):
